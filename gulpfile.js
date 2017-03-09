49348285e682f80e5b64e200f8aa84c4937a30f4
Volodymyr <balan.volodymyr@gmail.com>
2017-03-10 00:48:55 +0300

    Launched plugin: gulp-livereload

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,11 @@
 var gulp = require('gulp');
 var server = require('gulp-server-livereload');
+
+//server
+gulp.task('start', function() {
+  gulp.src('./app')
+    .pipe(server({
+      livereload: true,
+      open: true
+    }));
+});
