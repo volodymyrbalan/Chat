551f118288ca88a58581d009eb0bc3fa80212aec
Volodymyr <balan.volodymyr@gmail.com>
2017-03-15 12:38:55 +0100

    instaled plugin gulp-sass, added task for this plugin. Created folder with a file sass.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var server = require('gulp-server-livereload');
+var sass = require('gulp-sass');
 
 //server
 gulp.task('start', function() {
@@ -8,4 +9,11 @@ gulp.task('start', function() {
       livereload: true,
       open: true
     }));
+});
+
+//style
+gulp.task('style', function () {
+  return gulp.src('app/sass/**/*.sass')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('public/css'));
 });
